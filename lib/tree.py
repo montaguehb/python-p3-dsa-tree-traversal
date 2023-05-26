bc418d7d00def85275488bbf04b3dc0ed6dbae93
Historia Montague <historia.montague@outlook.com>
2023-05-26 15:32:18 -0600

    Finished Lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,11 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    nodes_to_visit = [self.root]
+    
+    while nodes_to_visit:
+      node = nodes_to_visit.pop()
+      if(node['id'] == id):
+        return node
+      else:
+        nodes_to_visit = node['children'] + nodes_to_visit
